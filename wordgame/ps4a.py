dd479dc0b19742fc4871abdd25cf6e3dc509b956
zachpuck <zachpuckett@gmail.com>
2018-03-25 23:36:20 -0700

    add playgame function

--- wordgame/ps4a.py
+++ wordgame/ps4a.py
@@ -274,16 +274,29 @@ def playGame(wordList):
  
     2) When done playing the hand, repeat from step 1    
     """
-    # TO DO ... <-- Remove this comment when you code this function
-    print("playGame not yet implemented.") # <-- Remove this line when you code the function
-   
+    playedYet = False
 
-
+    while True:
+        choice = input("Enter n to deal a new hand, r to replay the last hand, or e to end game: ")
+        
+        if choice == 'n':
+            hand = dealHand(HAND_SIZE)
+            playHand(hand, wordList, HAND_SIZE)
+            playedYet = True
+        elif choice == 'r':
+            if playedYet == False:
+                print("You have not played a hand yet. Please play a new hand first!")
+            else:
+                playHand(hand, wordList, HAND_SIZE)
+        elif choice == 'e':
+            break
+        else:
+            print("Invalid word, please try again.")
 
 #
 # Build data structures used for entire session and play game
 #
 if __name__ == '__main__':
     wordList = loadWords()
-    # playGame(wordList)
-    playHand({'e': 2, 'h': 1, 't': 2}, wordList, 5)
+    playGame(wordList)
+    # playHand({'e': 2, 'h': 1, 't': 2}, wordList, 5)
