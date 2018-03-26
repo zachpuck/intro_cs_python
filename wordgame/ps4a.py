01cc067f69874e385e0e2e3fd9042af678c7bf57
zachpuck <zachpuckett@gmail.com>
2018-03-25 22:02:40 -0700

    check hand length

--- wordgame/ps4a.py
+++ wordgame/ps4a.py
@@ -191,9 +191,7 @@ def calculateHandlen(hand):
     hand: dictionary (string-> int)
     returns: integer
     """
-    # TO DO... <-- Remove this comment when you code this function
-
-
+    return len(hand.keys())
 
 def playHand(hand, wordList, n):
     """
