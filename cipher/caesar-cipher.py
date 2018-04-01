797eeadf6db3cc6f263497e11fddcba0ab22eb6b
zachpuck <zachpuckett@gmail.com>
2018-04-01 16:49:45 -0700

    implement CiphertextMessage class

--- cipher/caesar-cipher.py
+++ cipher/caesar-cipher.py
@@ -215,7 +215,7 @@ class CiphertextMessage(Message):
             self.message_text (string, determined by input text)
             self.valid_words (list, determined using helper function load_words)
         '''
-        pass #delete this line and replace with your code here
+        Message.__init__(self, text)
 
     def decrypt_message(self):
         '''
@@ -233,7 +233,28 @@ class CiphertextMessage(Message):
         Returns: a tuple of the best shift value used to decrypt the message
         and the decrypted message text using that shift value
         '''
-        pass #delete this line and replace with your code here
+
+        high = 0
+        bestShift = 0
+
+        for s in range(26):
+            testDecrypt = self.apply_shift(26-s)
+            for w in testDecrypt.split(' '):
+                matchCount = 0
+                if is_word(self.valid_words, w):
+                    matchCount += 1
+
+            if matchCount > high:
+                high = matchCount
+                bestShift = s
+        
+        if bestShift == 0:
+            result = 0
+        else:
+            result = 26-bestShift
+
+        return (result, self.apply_shift(26-bestShift))
+
 
 #Example test case (PlaintextMessage)
 plaintext = PlaintextMessage('hello', 2)
@@ -245,8 +266,6 @@ ciphertext = CiphertextMessage('jgnnq')
 print('Expected Output:', (24, 'hello'))
 print('Actual Output:', ciphertext.decrypt_message())
 
-testMessage = 'hello'
-# print(testMessage.get_message_text())
-
-plaintest = PlaintextMessage(testMessage, 2)
-plaintest.get_encrypting_dict()
+# testMessage = 'jgnnq'
+# ciphertest = CiphertextMessage(testMessage)
+# ciphertest.decrypt_message()
